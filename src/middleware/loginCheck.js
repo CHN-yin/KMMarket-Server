4b81535449c4293cc4d3a2c8052cef50d4e41fd2
CHN-yin <shenghong2003.@gmail.com>
2023-11-10 16:57:23 +0800

    test：清除测试

--- src/middleware/loginCheck.js
+++ src/middleware/loginCheck.js
@@ -7,9 +7,7 @@ const { ErrorModel } = require('../res-model/index')
 module.exports = async (ctx, next) => {
   const session = ctx.session
   if(session && session.userInfo) {
-    console.log('userInfo', session.userInfo);
     return await next()
   }
-  console.log('userInfo', session.userInfo);
   ctx.body = new ErrorModel(10003, '请登录')
 }
